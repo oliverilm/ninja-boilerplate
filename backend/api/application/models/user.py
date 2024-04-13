ae75fd4a82fbc70a2a97e9ec871adf63b92965e4
oliver <oilmjarv@erply.com>
2024-04-13 23:40:09 +0300

    fix

--- backend/api/application/models/user.py
+++ backend/api/application/models/user.py
@@ -7,6 +7,9 @@ class UserGoogleProfle(models.Model):
     user_id = models.CharField(unique=True, max_length=255)
     name = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.email
+
 class AppUser(AbstractUser):
     email = models.EmailField(unique=True)
     google_profile = models.OneToOneField(
--- backend/api/application/utils/google.py
+++ backend/api/application/utils/google.py
@@ -32,23 +32,27 @@ def get_google_profile(access_token) -> GoogleProfile:
     except Exception as e:
         print("Error retrieving Google profile:", e)
         return None
-    
 
 
+# TODO: need to optimize the whole flow, its just a bit too slow
 def finalize_google_action(
         google_profile: UserGoogleProfle,
         created: bool
 ) -> TokenSchema | None:
     user = None
     if created:
+        names = google_profile.name.split(" ")
         user = get_user_model().objects.create_user(
                 username=google_profile.email,
                 email=google_profile.email,
                 google_profile=google_profile,
-                password=None
+                password=None,
+                last_name=names[-1],
+                first_name=" ".join(names[0:-1])
         )
     else:
         user = get_user_model().objects.get(email=google_profile.email)
+    
     if user is not None:
         return get_tokens_for_user(user)
     return None
